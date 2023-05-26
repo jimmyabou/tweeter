17f146d05fc79725ead656facd10aa6c0814d630
jamilaboudib <jimmy_aboudib@hotmail.com>
2023-05-25 20:51:43 -0400

    finalize fundamentals and style

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,77 +1,4 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 
-// Fake data taken from initial-tweets.json
-
-
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ];
-
-
-// const markup = `
-// <article class="tweet">
-// <header>
-//   <div class="person">
-//     <img src="${object.user.avatar}">
-//     <p>${object.user.name}</p>
-//   </div>
-//   <div>
-//     <p>"${object.user.handle}"</p>
-//   </div>
-// </header>
-
-// <div class="content">
-//   <p>
-//   ${object.content.text}
-//   </p>
-// </div>
-
-// <footer>
-//   <div>${object.created_at}</div>
-//   <div>
-//     <i class="fa-solid fa-flag"></i>
-//     <i class="fa-solid fa-retweet"></i>
-//     <i class="fa-solid fa-heart"></i>
-//   </div>
-
-// </footer>
-// </article>
-// `;
-
-const renderTweets = function (tweets) {
-  // loops through tweets
-  for (let tweet of tweets) {
-    // calls createTweetElement for each tweet
-    $('#section').prepend(createTweetElement(tweet));
-    // takes return value and appends it to the tweets container
-  }
-
-};
 const escape = function (str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
@@ -119,44 +46,39 @@ const createTweetElement = function (object) {
   </footer>
   </article>
   `);
-  // ...
-  // console.log($tweet);
   return $tweet;
 };
+
 $(document).ready(function () {
 
+  const renderTweets = function (tweets) {
+    for (let tweet of tweets) {
+      $('#section').prepend(createTweetElement(tweet));
+    }
+  
+  };
   function loadTweets() {
     $.ajax('/tweets', { method: 'GET' })
       .then(function (data) {
-        // console.log('Success: ', data);
         renderTweets(data);;
       });
   }
-  // renderTweets(data); 
 
-  // $('#form-tweet').submit(function(event) {
-  //   event.preventDefault();
   $("#form-tweet").on("submit", function (event) {
-    //  alert( "Handler for `submit` called." );
     event.preventDefault();
     const inputLength = $("#tweet-text").val().length;
     $('#tweets-container').find('#error').remove();
-    // console.log(input);
     if (inputLength === 0) {
       
       const text="No Input"
       $('#tweets-container').prepend(error(text));
-      // alert("No Input");
     }
     else if (inputLength > 140){
       const text=" Input exceeds length"
       $('#tweets-container').prepend(error(text));
-      // alert(" Input exceeds length");
     } else {
-      // $('#tweet-text').text();
       const data = $('form').serialize();
       
-      // data.text();
       console.log(data);
       $.ajax({
         url: 'http://localhost:8080/tweets',
@@ -164,6 +86,8 @@ $(document).ready(function () {
         data: data
       }).then(function(){
         $('output').html('140');
+
+        $('#section').empty();
         loadTweets();
         $('#tweet-text').val('');
         
@@ -171,11 +95,10 @@ $(document).ready(function () {
       )
      
     }
+    
   });
 
 
-  
+  loadTweets();
 }
 );
-
-// url: 'https://example.com/api/endpoint'
