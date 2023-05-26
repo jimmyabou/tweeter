9d2419518d79d61f1e584e00f6995b21655d7566
jamilaboudib <jimmy_aboudib@hotmail.com>
2023-05-25 21:35:37 -0400

    final

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,54 +1,20 @@
 
+//counts the characters in the text area input 
+// returns the remainig characters 
+// turns the counter color to red if input exceeds 140
 $(document).ready(function() {
   const tweet=$('#tweet-text');
-  
-  console.log("loaded");
-  // tweet.on('keyup',function(e){
-  //   let buttonValue = tweet.val();
-  //   console.log('Key up event:', buttonValue.length);
-  // });
   tweet.on('input', function(event) {
     let input = $(this).val();
-    let value=input.length;
-    // console.log('Key up event:', value);
-    // let label = $(this).find('output');
-    
+    let length=input.length;
     let label =$('.counter');
-    let result=140-value;
-    label.text(result);
-    if (result < 0) {
+    let remainder=140-length;
+    label.text(remainder);
+    if (remainder < 0) {
       label.css({'color':'red'});
     } else {
       label.css({'color': ''}); 
     }
-    // console.log('sibling:', label);
   });
-  // tweet.on('keydown', function(event) {
-  //   console.log('Key down event:', event.key);
-  // });
-  // tweet.on('change',function(e){
-  //   console.log('Key up event:', e.key);
-  // });
-  // tweet.on('input', function(event) {
-  //   console.log('Key press event:', event.key);
-  // });
-  
-  // tweet.on('blur', function(event) {
-  //   console.log('Key down event:', event.key);
-  // });
 
 });
-
-// const inputElement = document.getElementsByTagName('textarea')[0];
-
-// inputElement.addEventListener('keyup', function(event) {
-//   console.log('Key up event:', event.key);
-// });
-
-// inputElement.addEventListener('keypress', function(event) {
-//   console.log('Key press event:', event.key);
-// });
-
-// inputElement.addEventListener('keydown', function(event) {
-//   console.log('Key down event:', event.key);
-// });
